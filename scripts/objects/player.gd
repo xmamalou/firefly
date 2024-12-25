a8f7bbcc81a3bca1e7c6ccf92393a6dc4c69ba16
Christopher-Marios Mamaloukas <xmamalou@cyber-mantid.xyz>
2024-12-25 17:54:53 +0200

    Added collision, made movement bit more sophisticated

--- scripts/objects/player.gd
+++ scripts/objects/player.gd
@@ -1,37 +1,79 @@
-extends Node3D
+class_name Player
+extends CharacterBody3D
 
 @export_category("Movement")
 @export_range(1, 40)
-var speed: int = 10
+var ground_speed: int = 10 ## Specifies the speed of the player when moving on the ground, in [code]meters/sec[/code]
+@export_range(1, 10)
+var vertical_speed: int = 2 ## specifies the speed of the player when jumping, in [code]meters/sec[/code]
+@export_range(1, 10, 1)
+var gravity_pull: int = 10 ## Specifies the strength of a homogenous gravitational field pulling "down", in [code]meters/sec^2[/code]
 
-@onready
-var camera := $center/camera as Camera3D;
-@onready
-var mesh := $mesh as MeshInstance3D
+# Public variables
+var spawn_point: Node3D ## Specifies a spawn point for the player, in case they are killed
+
+# Private variables
+#var _tolerance_margin: float = 0.05 ## Specifes a margin where the player is considered to move "practically" orthogonally to a collision object 
+
+# Onready variables
+@onready var camera := $center/camera as PlayerCamera
+@onready var center := $center as Node3D
+@onready var mesh := $mesh as MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var front_vector := (mesh.position - Vector3(
-			camera.position.x,
-			mesh.position.y,
-			camera.position.z)).normalized()
-	var perp_vector := front_vector.cross(Vector3(0, 1, 0))
+func _process(_delta: float) -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	var displacement := Vector3(0, 0, 0)
+	# Displacement due to ground speed and gravity
+	displacement = self.velocity*delta - 0.5*self.gravity_pull*delta**2*Vector3(0, 1, 0)
+	# Velocity increase (or decrease) due to gravity
+	self.velocity.y -= self.gravity_pull*delta
 	
-	if Input.is_action_pressed("move_front"):
-		self.position += self.speed*delta*front_vector
-	if Input.is_action_pressed("move_back"):
-		self.position -= self.speed*delta*front_vector
-	if Input.is_action_pressed("move_left"):
-		self.position -= self.speed*delta*perp_vector
-	if Input.is_action_pressed("move_right"):
-		self.position += self.speed*delta*perp_vector
+	var collision: KinematicCollision3D = self.move_and_collide(displacement)
+	# Player collides with something
+	if collision:
+		# Player cannot move downwards anymore
+		# TODO: Allow player to fall if not falling *on* ground
+		# TODO: Maybe add bounce as well - instead of zeroing, sets velocity to some amount
+		self.velocity.y = 0
+		# Player is on the ground - they can walk around and perform a jump
+		if (collision.get_collider() as Node3D).is_in_group("ground"):
+			# Vectors on the xz plane
+			var front_vector := (center.position - Vector3(
+					camera.position.x,
+					center.position.y,
+					camera.position.z)).normalized()
+			var _normal := collision.get_normal()
+			var perp_vector := front_vector.cross(collision.get_normal()).normalized()
+			var plane_front_vector := perp_vector.cross(collision.get_normal()).normalized()
+			# Movement on the ground
+			var movement: Vector2 = Input.get_vector(
+					"move_back", 
+					"move_front", 
+					"move_right", 
+					"move_left")
+			self.velocity = -self.ground_speed*( movement.x*plane_front_vector 
+					+ movement.y*perp_vector )
+			# Jump
+			if Input.is_action_pressed("jump"):
+				self.velocity.y = self.vertical_speed
+		# Kill plane
+		if (collision.get_collider() as Node3D).is_in_group("kill"):
+			self.velocity = Vector3(0, 0, 0)
+			self.position = self.spawn_point.position if self.spawn_point \
+					else Vector3(0, 0, 0)
+	else:
+		pass
 
 func _input(event: InputEvent) -> void:
+	 # Exit the game
 	if event is InputEventKey and (event as InputEventKey).is_action_pressed("quit"):
-		self.get_tree().quit() # Exit
-		
+		self.get_tree().quit()
--- scripts/objects/player/player_camera.gd
+++ scripts/objects/player/player_camera.gd
@@ -1,8 +1,11 @@
-extends Camera3D
 class_name PlayerCamera
+extends Camera3D
+
+signal camera_displaced(pos: Vector3)
 
 @export_range(0.01, 0.09, 0.001)
 var motion_subdivide := 0.03 ## Subdivision amount of the relative motion of the mouse.
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,13 +16,16 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 	
+	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var change := (event as InputEventMouseMotion).relative
 		var d_azimuth := self.motion_subdivide*change.x
 		var d_alt := self.motion_subdivide*change.y
-		self.rotate_cam(
+		self._rotate_cam(
 				Vector2(d_azimuth, d_alt))
+		self.camera_displaced.emit(self.position)
+
 
 ## [font=res://assets/fonts/inter.ttf]
 ## Rotate the camera according to an azimuth and altitude angle
@@ -27,13 +33,19 @@ func _input(event: InputEvent) -> void:
 ## [b]PARAMETERS[/b][br]
 ## - [param d_angle]: a 2D vector, where [param x] is the azimuth angle and [param y] the altitude angle
 ## [/font]
-func rotate_cam(d_angle: Vector2) -> void:
-	#var cam_plane_angle := atan(self.position.x/self.position.z)
+func _rotate_cam(d_angle: Vector2) -> void:
+	var future_theta: float = acos(Vector3(self.position.x, 0, self.position.z).normalized().dot(
+			self.position.normalized())) + (d_angle.y if GameOptions.is_cam_inverted else -d_angle.y)
 	# Rotation
 	self.rotation.y += d_angle.x
-	#self.rotation.x += cos(cam_plane_angle)*d_angle.y
-	#self.rotation.z += sin(cam_plane_angle)*d_angle.y
+	self.rotation.x -= ( 
+			d_angle.y if abs(future_theta) <= PI/3 and future_theta >= 0
+			else 0.0) * (1.0 if GameOptions.is_cam_inverted else -1.0)
 	# Position
-	var plane_vec := Vector3(self.position.x, 0, self.position.z).rotated(
-			Vector3(0, self.position.y, 0).normalized(), d_angle.x)
-	self.position = Vector3(plane_vec.x, self.position.y, plane_vec.z)
+	var plane_vec: Vector3 = Vector3(self.position.x, self.position.y, self.position.z).rotated(
+			Vector3(0, self.position.y, 0).normalized(), d_angle.x).rotated(
+					Vector3(self.position.x, 0, self.position.z).normalized().cross(
+							Vector3(0, 1, 0)), 
+							(d_angle.y if abs(future_theta) <= PI/3 and future_theta >= 0
+							else 0.0) * (1.0 if GameOptions.is_cam_inverted else -1.0))
+	self.position = Vector3(plane_vec.x, plane_vec.y, plane_vec.z)
