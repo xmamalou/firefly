d6d72c454f0f50e6771d5434965df9be557306e7
Christopher-Marios Mamaloukas <xmamalou@cyber-mantid.xyz>
2024-12-26 20:29:02 +0200

    Added mouse sensitivity for camera

--- globals/game_options.gd
+++ globals/game_options.gd
@@ -1,4 +1,5 @@
 extends Node
 
 # Input
-static var is_cam_inverted := false ## [code]false[/code]: camera follows mouse movement [br] [code]true[/code]: camera follows reverse mouse movement
+static var is_cam_inverted := true ## [code]false[/code]: camera follows mouse movement [br] [code]true[/code]: camera follows reverse mouse movement
+static var mouse_sensitivity := 5 ## How sensitive the camera is to mouse movements
--- scripts/objects/player/player_camera.gd
+++ scripts/objects/player/player_camera.gd
@@ -3,9 +3,12 @@ extends Camera3D
 
 signal camera_displaced(pos: Vector3)
 
-@export_range(0.01, 0.09, 0.001)
-var motion_subdivide := 0.03 ## Subdivision amount of the relative motion of the mouse.
-
+@export var is_inverted: bool = false ## If set to true, camera is inverted
+@export_range(1, 100, 1, "suffix:%")
+var sensitivity: float = 5 : ## How sensitive the camera is to mouse movements
+	get: return sensitivity/1000
+	set(value): # sensitivity is expressed as a whole percentage (1 to 100, no decimals)
+		sensitivity = max(1, abs(ceil(value))) # if value == 0, then 1 is chosen instead
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,8 +23,8 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		var change := (event as InputEventMouseMotion).relative
-		var d_azimuth := self.motion_subdivide*change.x
-		var d_alt := self.motion_subdivide*change.y
+		var d_azimuth := self.sensitivity*change.x
+		var d_alt := self.sensitivity*change.y
 		self._rotate_cam(
 				Vector2(d_azimuth, d_alt))
 		self.camera_displaced.emit(self.position)
@@ -35,17 +38,17 @@ func _input(event: InputEvent) -> void:
 ## [/font]
 func _rotate_cam(d_angle: Vector2) -> void:
 	var future_theta: float = acos(Vector3(self.position.x, 0, self.position.z).normalized().dot(
-			self.position.normalized())) + (d_angle.y if GameOptions.is_cam_inverted else -d_angle.y)
+			self.position.normalized())) + (d_angle.y if self.is_inverted else -d_angle.y)
 	# Rotation
 	self.rotation.y += d_angle.x
 	self.rotation.x -= ( 
 			d_angle.y if abs(future_theta) <= PI/3 and future_theta >= 0
-			else 0.0) * (1.0 if GameOptions.is_cam_inverted else -1.0)
+			else 0.0) * (1.0 if self.is_inverted else -1.0)
 	# Position
 	var plane_vec: Vector3 = Vector3(self.position.x, self.position.y, self.position.z).rotated(
 			Vector3(0, self.position.y, 0).normalized(), d_angle.x).rotated(
 					Vector3(self.position.x, 0, self.position.z).normalized().cross(
 							Vector3(0, 1, 0)), 
 							(d_angle.y if abs(future_theta) <= PI/3 and future_theta >= 0
-							else 0.0) * (1.0 if GameOptions.is_cam_inverted else -1.0))
+							else 0.0) * (1.0 if self.is_inverted else -1.0))
 	self.position = Vector3(plane_vec.x, plane_vec.y, plane_vec.z)
