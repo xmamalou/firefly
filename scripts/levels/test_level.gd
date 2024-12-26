75f64d60cd4396dc1ee22c6352d4ff435a7e8887
Christopher-Marios Mamaloukas <xmamalou@cyber-mantid.xyz>
2024-12-26 20:31:17 +0200

    Delegated spawn point assignment from scene script to scene resource

--- scripts/levels/test_level.gd
+++ scripts/levels/test_level.gd
@@ -5,7 +5,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.spawn_point = spawn_point
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
