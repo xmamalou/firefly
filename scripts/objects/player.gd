b2758aa8b85ebc11177cf21561a1444e85539cdf
Christopher-Marios Mamaloukas <xmamalou@cyber-mantid.xyz>
2024-12-29 14:23:28 +0200

    Made gravity into vector and added bouncing

--- scripts/objects/player.gd
+++ scripts/objects/player.gd
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 class_name Player
 extends CharacterBody3D
 ## A class representing a player controllable by the user
@@ -24,26 +23,43 @@ extends CharacterBody3D
 signal speed_changed(old_speed: Vector3, new_speed: Vector3)
 
 #region Exports
-@export_category("Movement")
-@export_range(1, 40, 1, "suffix:m/sec") var ground_speed: int = 10 ## Specifies the speed of the player when moving on the ground, in SI units
-@export_range(1, 10, 1, "suffix:m/sec") var vertical_speed: int = 2 ## Specifies the speed of the player when jumping, in SI units
-@export_range(1, 10, 1, "suffix:m/sec^2") var gravity_pull: int = 10 ## Specifies the strength of a homogenous gravitational field pulling "down", in SI units
-@export var spawn_point: Node3D ## Specifies a spawn point for the player, in case they are killed
+@export_group("Movement")
+## Specifies the speed of the player when moving on the ground, in SI units
+@export_range(1, 40, 1, "suffix:m/sec") var ground_speed: int = 10 
+## Specifies the speed of the player when jumping, in SI units
+@export_range(1, 10, 1, "suffix:m/sec") var vertical_speed: int = 2 
+## Specifies the gravity [b]acceleration[/b] vector, in SI units
+@export_custom(PROPERTY_HINT_ARRAY_TYPE, "suffix: m/sec^2") var gravity_pull := Vector3(0, -10, 0)
+## Specifies a spawn point for the player, in case they are killed
+@export var spawn_point: Node3D 
+# ------------------------
+@export_group("Camera")
+## If set to true, camera is inverted
+@export var is_cam_inverted: bool = false 
+## How sensitive the camera is to mouse movements
+@export_range(1, 100, 1, "suffix:%") var mouse_sensitivity: float = 5 
 #endregion
 
-# Private variables
-#var _tolerance_margin: float = 0.05 ## Specifes a margin where the player is considered to move "practically" orthogonally to a collision object 
+#region Private variables
+## Specifes a margin where the player is considered to move "practically" orthogonally to a collision object 
+var _tolerance_margin: float = 0.8
+#endregion
 
-#region Onready variables
-@onready var camera := $center/camera as PlayerCamera ## The camera the player controls
-@onready var center := $center as Node3D ## The center that the camera follows
-@onready var mesh := $mesh as MeshInstance3D ## The mesh representing the player
+#region Subnodes
+## The camera the player controls
+@onready var _camera := $center/camera as PlayerCamera
+## The center that the camera follows
+@onready var _center := $center as Node3D 
+#@onready var _mesh := $mesh as MeshInstance3D ## The mesh representing the player
 #endregion
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	self.position = self.spawn_point.position
+	
+	self._camera.is_inverted = self.is_cam_inverted
+	self._camera.sensitivity = self.mouse_sensitivity
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -57,30 +73,32 @@ func _physics_process(delta: float) -> void:
 	
 	var displacement := Vector3(0, 0, 0)
 	# Displacement due to ground speed and gravity
-	displacement = self.velocity*delta - 0.5*self.gravity_pull*delta**2*Vector3(0, 1, 0)
+	displacement = self.velocity*delta + 0.5*self.gravity_pull*delta**2
 	# Velocity increase (or decrease) due to gravity
-	self.velocity.y -= self.gravity_pull*delta
+	self.velocity += self.gravity_pull*delta
 	
 	var collision: KinematicCollision3D = self.move_and_collide(displacement)
 	# Player collides with something
 	if collision:
+		# We want to push the player on the opposite direction
+		self.velocity = -collision.get_remainder().normalized()
+		var collider := collision.get_collider() as Node3D
 		# Kill plane
-		if (collision.get_collider() as Node3D).is_in_group("kill"):
+		if collider.is_in_group("kill"):
 			self.velocity = Vector3(0, 0, 0)
 			self.position = (
 					self.spawn_point.position if self.spawn_point
 					else Vector3(0, 0, 0))
 		# Player cannot move downwards anymore
-		# FIXME: Allow player to fall if not falling *on* ground
-		# TODO: Maybe add bounce as well - instead of zeroing, sets velocity to some amount
-		self.velocity.y = 0
 		# Player is on the ground - they can walk around and perform a jump
-		if (collision.get_collider() as Node3D).is_in_group("ground"):
+		if collider.is_in_group("ground"): # objects in the `ground` group are GroundBody objects, hence they have the `bounce` property 
+			self.velocity *= (collider as GroundBody).bounce*(collider as GroundBody).dissipation*(
+					0.0 if abs(self.velocity.y) < self._tolerance_margin else 1.0) # This is to prevent infinite bouncing
 			# Vectors on the xz plane
-			var front_vector := (self.center.position - Vector3(
-					self.camera.position.x,
-					self.center.position.y,
-					self.camera.position.z)).normalized()
+			var front_vector := (self._center.position - Vector3(
+					self._camera.position.x,
+					self._center.position.y,
+					self._camera.position.z)).normalized()
 			var perp_vector := front_vector.cross(collision.get_normal()).normalized()
 			var plane_front_vector := perp_vector.cross(collision.get_normal()).normalized()
 			# Movement on the ground
@@ -89,7 +107,7 @@ func _physics_process(delta: float) -> void:
 					"move_front", 
 					"move_right", 
 					"move_left")
-			self.velocity = -self.ground_speed*( movement.x*plane_front_vector 
+			self.velocity += -self.ground_speed*( movement.x*plane_front_vector 
 					+ movement.y*perp_vector )
 			# Jump
 			if Input.is_action_pressed("jump"):
